b735daff01510a7df7a1c25f34007fc7b7ecdfa2
Eugene <36137811+Throttler@users.noreply.github.com>
2019-11-13 10:17:27 +0400

    Create outfunc.cpp

---
+++ outfunc.cpp
@@ -0,0 +1,6 @@
+#include <string>
+
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
+	((string *)userp)->append((char *)contents, size * nmemb);
+	return size * nmemb;
+}
