f51f75334fb6206de93b44b8f1dfb012e1e2c3a0
Eugene <36137811+Throttler@users.noreply.github.com>
2019-11-13 11:36:49 +0400

    Delete outfunc.cpp

--- outfunc.cpp
+++
@@ -1,6 +0,0 @@
-#include <func.h>
-
-size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp){
-	((string *)userp)->append((char *)contents, size * nmemb);
-	return size * nmemb;
-}
